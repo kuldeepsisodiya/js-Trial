4cb3dde569c3365cd1ca2e19db4f2129b898e073
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-17 16:40:59 +0000

    function and parameters

--- functions.js
+++ functions.js
@@ -3,10 +3,20 @@
 // }
 // say()
 
-function sum(a,b){
-    return(a+b)
-    //console.log(a+b) dont save in any other variable  
-}
+// function sum(a,b){ // (a,b) are the parameters 
+//     return(a+b)
+//     //console.log(a+b) dont save in any other variable  
+//     //also after return any of console or operation dosnt counted 
+// }
 
-const result = sum(1,6)
-console.log(result);
+// const result = sum(1,6)
+// console.log(result);
+
+function userloginmsg(name){
+    if (name == undefined){
+        console.log("Please enter a username")
+        return
+    }
+    return `${name} just logged in`
+}
+console.log(userloginmsg())
