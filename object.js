dae5fcd35985f3e8d84c2865c7bcbdf7c177a377
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-17 13:25:04 +0000

    7/17/2026, 1:25:04 PM - 1 files changed

--- object.js
+++ object.js
@@ -1,14 +1,38 @@
-const mySmbol = Symbol("mykey")
-const jsuser = {
-    name: "Kuldeep",
-    [mySmbol]: "mykeh",
-    age: 19,
-    location: "Indore",
-    isLoggedIn: false
-}
-console.log(jsuser[mySmbol]);
-console.log(typeof jsuser[mySmbol]);
-console.log(jsuser)
+// const mySmbol = Symbol("mykey")
+// const jsuser = {
+//     name: "Kuldeep",
+//     [mySmbol]: "mykeh",
+//     age: 19,
+//     location: "Indore",
+//     isLoggedIn: false
+// }
+// console.log(jsuser[mySmbol]);
+// console.log(typeof jsuser[mySmbol]);
+// console.log(jsuser)
 
-Object.freeze(jsuser)
-// dosnt commit the changes after this the js user remains same 
+// Object.freeze(jsuser)
+// // dosnt commit the changes after this the js user remains same 
+
+// const tinderUser = {}
+
+// tinderUser.id ="ad01"
+// console.log(tinderUser)
+
+// const regularUser ={
+//     email: "some@gmail.in",
+//     fullname: {
+//         username: {
+//             firstname: "ad",
+//             lastname: "singh"
+//         }
+//     }
+// }
+// console.log(regularUser.fullname.username.firstname);
+
+
+const obj1 ={1:"a",2:"b"}
+const obj2 ={1:"a",2:"b"}
+const obj3 = Object.assign({}, obj1, obj2)
+
+console.log(obj3);
+
