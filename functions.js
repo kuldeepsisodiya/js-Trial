a77324dd3834a714856bb48477682aec5fc768fc
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-17 16:33:40 +0000

    7/17/2026, 4:33:40 PM - 1 files changed

---
+++ functions.js
@@ -0,0 +1,12 @@
+// function say() {
+//     console.log("Hello Sir")
+// }
+// say()
+
+function sum(a,b){
+    return(a+b)
+    //console.log(a+b) dont save in any other variable 
+}
+
+const result = sum(1,6)
+console.log(result);
