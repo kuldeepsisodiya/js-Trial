d90278ea09be5785d4b1f80e6d83568882fc84f6
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-17 16:25:31 +0000

    7/17/2026, 4:25:31 PM - 1 files changed

---
+++ api-objects.js
@@ -0,0 +1,19 @@
+const user = {
+    username: "kuldeep",
+    usernitro: true,
+    userdeco: true
+}
+//user.username
+const {username} = user 
+console.log(username);
+
+//json formating
+{
+    "username"= "kuldeep"
+    "ai"="grok"
+}
+//in arry
+[
+    {},
+    {}
+]
