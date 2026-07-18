935f91870484129ed53b8f8ac00c99eb1dfcb994
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-18 13:47:48 +0000

    7/18/2026, 1:47:48 PM - 1 files changed

--- functions.js
+++ functions.js
@@ -12,11 +12,30 @@
 // const result = sum(1,6)
 // console.log(result);
 
-function userloginmsg(name){
-    if (name == undefined){
-        console.log("Please enter a username")
-        return
-    }
-    return `${name} just logged in`
+// function userloginmsg(name){
+//     if (name == undefined){
+//         console.log("Please enter a username")
+//         return
+//     }
+//     return `${name} just logged in`
+// }
+// console.log(userloginmsg())
+
+const user = {
+    username: "user",
+    prices: 199
 }
-console.log(userloginmsg())
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+// handleObject({
+    // username:"user2",
+    // price: 233
+// })
+
+const newArry = [200,400,100,600]
+function returnsec(getArray){
+    return getArray[i]
+}
+console.log(newArry);
