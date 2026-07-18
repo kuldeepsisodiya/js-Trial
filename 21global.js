fdb76327e08666215282b78ce468e3f15dc9c65e
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-18 13:53:25 +0000

    7/18/2026, 1:53:25 PM - 1 files changed

---
+++ 21global.js
@@ -0,0 +1,9 @@
+var c = 300
+if (true){
+    let a = 10
+    const b = 30
+    c = 40 //or var c =40
+}
+console.log(c);
+//var or direct varible in any condition overides the orignal value of the varible var c=or c=
+//thats why we avoid var and direct variable define 
