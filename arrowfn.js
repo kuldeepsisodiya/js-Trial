875f573278827576769c4a909a2b83301dc14120
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-18 17:26:44 +0000

    arrow functions

--- arrowfn.js
+++ arrowfn.js
@@ -1,4 +1,27 @@
-const user = {
-    username: "hitesh",
-    price: 203
-}
+// const user = {
+//     username: "hitesh",
+//     price: 203,
+
+//      welcomemsg: function () {
+//         console.log(`${this.username} ,212`);
+//         }
+// }
+// user.welcomemsg()
+
+// const none = () => {
+//     let user = "None"
+//     console.log(this.user)
+// }
+// none()
+// const add = (a,b)=> {
+//     return a+b
+// }
+// console.log(add(3,4));
+//implesit return 
+
+// const add = (a,b)=> a+b
+// //or
+// // const add = (a,b) => (a+b)
+// console.log(add(3,4));
+
+// const add = (a,b) => ({username: "none"}) (for object return)
