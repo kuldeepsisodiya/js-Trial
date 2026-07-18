10cbadc72eef54ebcc54ba78e23449f3fee7194f
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-18 17:35:08 +0000

    arrow function with iife

--- invokedfn.js
+++ invokedfn.js
@@ -3,6 +3,14 @@
 //    console.log(`DB Connected`);
    
 // }) ()
+// ; to end the 
 
 // () () ()
 // first function define  2nd exicution call 
+
+//arrow function 
+(
+    () => {
+        console.log("db Nonee")
+    }
+)();
