647c019d81175609f2533e8dd6d47fcfe17f97cb
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-19 13:34:59 +0000

    7/19/2026, 1:34:59 PM - 1 files changed

---
+++ stck.js
@@ -0,0 +1,12 @@
+// global execution context [{}] (this.) 
+// function execution context
+// eval execution context
+
+// [{}] run in two phases 
+// 1st -> memeory creation phase (creation phase)
+// 2nd -> execution phase
+//
+
+//for example we take a code how it execute
+// 1st -> global execution ->this
+// 2nd  -> memory phase 
