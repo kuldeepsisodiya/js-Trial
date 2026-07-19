129504926000e3d0ca77d9fa134bd0902d61c8ac
Kuldeep Singh Sisodiya <108626384+kuldeepsisodiya@users.noreply.github.com>
2026-07-19 15:30:51 +0000

    7/19/2026, 3:30:51 PM - 1 files changed

--- loops.js
+++ loops.js
@@ -1,8 +1,20 @@
 
-array = [1,2,3,5,7]
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    console.log(element);
+// array = [1,2,3,5,7]
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element);
+// }
+
+//while
+// let arr = ["none","jidm"]
+// let condition = 0
+// while (condition < arr.length) {
+//     console.log(`Value of ${arr[condition]}`)
+//     condition = condition + 1
+// }
+// let score = 1 
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
     
-    
-}
+// } while (score<=11);
